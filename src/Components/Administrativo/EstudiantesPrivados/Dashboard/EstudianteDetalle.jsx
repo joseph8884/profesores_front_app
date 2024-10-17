3bdc902ac36f69f7cbb4f593589a77da2264acc0
jose <josegzm08@gmail.com>
2024-10-17 10:44:32 -0500

    Dashboard structure is done

--- src/Components/Administrativo/EstudiantesPrivados/Dashboard/EstudianteDetalle.jsx
+++ src/Components/Administrativo/EstudiantesPrivados/Dashboard/EstudianteDetalle.jsx
@@ -2,16 +2,47 @@ import React from "react";
 import { useLocation } from "react-router-dom";
 import NavMobile from "../../Nav/NavMobile";
 import NavWeb from "../../Nav/NavWeb";
+import { Button } from "../../../ui/button";
+import "./EstudianteDetalle.css";
 
 const StudentDetail = () => {
   const location = useLocation();
-  const studentData = JSON.parse(decodeURIComponent(new URLSearchParams(location.search).get("data")));
+  const studentData = JSON.parse(
+    decodeURIComponent(new URLSearchParams(location.search).get("data"))
+  );
 
   return (
     <div className="min-h-screen flex">
       <NavMobile />
       <NavWeb />
-      <h1>Student Detail</h1>
+      <div className="dashboard">
+        <div className="dashboardcontainer">
+          <div className="filtrosandbackbtn">
+            <a href="/estudiantes_privados_administrativo"><Button>Back</Button></a>
+            {
+            //Cosas de los filtros.
+            }
+          </div>
+          <div className="resumenDeActividadAcademica">
+            datos de la persona
+          </div>
+          <div className="calendario">
+            Calendario
+          </div>
+          <div className="grafica">
+            grafica
+          </div>
+          <div className="informacionDetalladaEstudiante">
+            info del estudiante
+          </div>
+          <div className="ultimasclasesvistas">
+            clases vistas
+          </div>
+        </div>
+      </div>
+
+      {/**
+         *       <h1>Student Detail</h1>
       <p>Name: {studentData.name}</p>
       <p>Virtual: {studentData.virtual}</p>
       <p>Presencial: {studentData.presencial}</p>
@@ -20,6 +51,7 @@ const StudentDetail = () => {
       <p>Horas Restantes: {studentData.horasRestantes}</p>
       <p>Cancelados Tarde: {studentData.canceladosTarde}</p>
       <p>Cancelados A Tiempo: {studentData.canceladosATiempo}</p>
+         */}
     </div>
   );
 };
